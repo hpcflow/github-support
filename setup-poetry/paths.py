a36cdac2b9976c16d115ebc177a5430292e6a620
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2025-01-29 09:36:15 +0000

    Ugh naming
    Moved some Python scriptlets into their own files.

---
+++ setup-poetry/paths.py
@@ -0,0 +1,3 @@
+import sys
+print(f"python={sys.executable}")
+print(f"poetry={sys.prefix}")
---
+++ setup-poetry/ver.py
@@ -0,0 +1,2 @@
+import sys
+print(f"py={sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
