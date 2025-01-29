204e9f1c4349384e10aecf1887e3ddf551a8bd4d
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2025-01-29 13:18:42 +0000

    Clean up

--- setup-poetry/paths.py
+++ setup-poetry/paths.py
@@ -1,3 +1,6 @@
+"""
+Print some key paths in a form that Github Actions can consume.
+"""
 import sys
 print(f"python={sys.executable}")
 print(f"poetry={sys.prefix}")
--- setup-poetry/ver.py
+++
@@ -1,2 +0,0 @@
-import sys
-print(f"py={sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
